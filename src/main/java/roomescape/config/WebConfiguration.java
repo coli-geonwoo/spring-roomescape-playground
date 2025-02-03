1cf1d5a2d5a2fdc0da5cb70342a962c927b7e3f1
coli <kkwoo001021@naver.com>
2025-02-03 20:48:13 +0900

    feat: 홈 화면 반환 기능 구현

---
+++ src/main/java/roomescape/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package roomescape.config;
+
+import java.time.Duration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/", "classpath:/templates/")
+                .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
+    }
+}
+
